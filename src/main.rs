11a472c6b14cb170f7a2ed6ba488861e0b2883ae
Nnamani John <nnamanijohn123@gmail.com>
2024-12-17 18:32:54 +0100

    positive or negative

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-use std::io::{stdout, Write, stdin, Read};
 
 fn main() {
-    let mut buffer: [u8; 1000] = [0; 1000];
-
-    match stdin().read(&mut buffer) {
-        Ok(bytes) => {
-            println!("Read {} bytes from stdin", bytes);
-            let input = &buffer[0..bytes];
-
-            match stdout().write(input) {
-                Ok(bytes) => println!("written {} bytes to stdout", bytes),
-                Err(_) => eprintln!("Could not write to stdout")
-            }
-        }
-        Err(_) => eprintln!("Could not read from stdin")
+    let random_number = rand::random::<i32>();
+    
+    if random_number > 0  {
+        println!("{random_number} is positive");
+    } else if random_number < 0 {
+        println!("{random_number} is negative");
+    } else {
+        println!("{random_number} is zero");
     }
 }
