a884f1a9fa6ed728ab7288a0bccbd9e38c24b09b
Nnamani John <nnamanijohn123@gmail.com>
2024-12-17 19:58:42 +0100

    print alphabets

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ fn main() {
     const ALPHABET_COUNT: usize = 26;
     const NEWLINE: u8 = b'\n';
 
-    let mut buffer: [u8; ALPHABET_COUNT * 2 + 1] = [0; ALPHABET_COUNT * 2 + 1];
+    let mut buffer: [u8; ALPHABET_COUNT + 1] = [0; ALPHABET_COUNT + 1];
 
     for i in 0..ALPHABET_COUNT {
+        if b'a' + i as u8 == b'e' || b'a' + i as u8 == b'q' {
+            continue;
+        }
         buffer[i] = b'a' + i as u8;
-        buffer[ALPHABET_COUNT + i] = b'A' + i as u8;
     }
 
-    buffer[ALPHABET_COUNT * 2] = NEWLINE;
+    buffer[ALPHABET_COUNT] = NEWLINE;
 
     stdout()
         .write_all(&buffer)
