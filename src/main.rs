293010338aad9abd83060ab7051b3146ebf17243
Nnamani John <nnamanijohn123@gmail.com>
2024-12-17 19:40:41 +0100

    print alphabets

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-fn main() {
-    let random_number = rand::random::<u32>();
-    let last_digit = random_number % 10;
+use std::io::{stdout, Write};
 
-    print!("Last digit of {random_number} is {last_digit} ");
-    if last_digit > 5 {
-        print!("and is greater than 5\n")
-    } else if last_digit > 0 {
-        print!("and is less than 6 and not 0\n")
-    } else {
-        print!("and is 0\n")
+fn main() {
+    const ALPHABET_COUNT: usize = 26;
+    const NEWLINE: u8 = b'\n';
+
+    let mut buffer: [u8; ALPHABET_COUNT + 1] = [0; ALPHABET_COUNT + 1];
+
+    for i in 0..ALPHABET_COUNT {
+        buffer[i] = b'a' + i as u8;
     }
+
+    buffer[ALPHABET_COUNT] = NEWLINE;
+
+    stdout()
+        .write_all(&buffer)
+        .expect("Error writing to stdout");
 }
