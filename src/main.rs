47b330476f1c339959a2d6037176557b05443296
Nnamani John <nnamanijohn123@gmail.com>
2024-12-18 12:38:04 +0100

    print comb3

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 use std::io::{stdout, Write};
 
 fn main() {
-    const TOTAL_COUNT: usize = 9;
+    const TOTAL_COUNT: usize = 8;
     const SPACE: u8 = b' ';
     const COMMA: u8 = b',';
     const FIRST_DIGIT: u8 = b'0';
-    const TOTAL_DIGITS: usize = (TOTAL_COUNT * (TOTAL_COUNT + 1)) / 2;
-    const BUFFER_SIZE: usize = TOTAL_DIGITS * 4 - 1;
+    const TOTAL_DIGITS: usize = (TOTAL_COUNT * (TOTAL_COUNT + 1) * (TOTAL_COUNT + 2)) / 6;
+    const BUFFER_SIZE: usize = TOTAL_DIGITS * 5 - 1;
 
     let mut buffer = [0u8; BUFFER_SIZE];
     let mut index = 0;
 
     for i in 0..TOTAL_COUNT {
         for j in i + 1..=TOTAL_COUNT {
-            buffer[index] = FIRST_DIGIT + i as u8;
-            buffer[index + 1] = FIRST_DIGIT + j as u8;
+            for k in j + 1..=TOTAL_COUNT + 1 {
+                buffer[index] = FIRST_DIGIT + i as u8;
+                buffer[index + 1] = FIRST_DIGIT + j as u8;
+                buffer[index + 2] = FIRST_DIGIT + k as u8;
 
-            if index + 4 < BUFFER_SIZE {
-                buffer[index + 2] = COMMA;
-                buffer[index + 3] = SPACE;
-                index += 4;
-            } else {
-                index += 2;
+                if index + 5 < BUFFER_SIZE {
+                    buffer[index + 3] = COMMA;
+                    buffer[index + 4] = SPACE;
+                    index += 5;
+                } else {
+                    index += 3;
+                }
             }
         }
     }
