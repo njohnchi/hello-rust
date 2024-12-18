42013988cbb6a89debb2f073c287e80380d7937f
Nnamani John <nnamanijohn123@gmail.com>
2024-12-18 06:33:54 +0100

    print comb

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use std::io::{stdout, Write};
 
 fn main() {
-    const TOTAL_COUNT: usize = 16;
-    let mut buffer = [0u8; TOTAL_COUNT + 1];
+    const TOTAL_COUNT: usize = 10;
+    const SPACE: u8 = b' ';
+    const COMMA: u8 = b',';
+    const FIRST_DIGIT: u8 = b'0';
+    const BUFFER_SIZE: usize = TOTAL_COUNT * 3 - 1;
 
-    for i in 0..TOTAL_COUNT {
-        if i < 10 {
-            buffer[i] = b'0' + i as u8;
-        } else {
-            buffer[i] = b'a' + (i % 10) as u8;
-        }
+    let mut buffer = [0u8; BUFFER_SIZE];
+
+    buffer[0] = FIRST_DIGIT;
+
+    for i in 0..TOTAL_COUNT - 1 {
+        let index = i * 3;
+        buffer[index + 1] = COMMA;
+        buffer[index + 2] = SPACE;
+        buffer[index + 3] = FIRST_DIGIT + 1 + i as u8;
     }
-    buffer[TOTAL_COUNT] = b'\n';
+
+    buffer[BUFFER_SIZE - 1] = b'\n';
 
     stdout()
         .write_all(&buffer)
