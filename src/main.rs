92b5a5b90c5faad4b9623cb35692c5791dc1a989
Nnamani John <nnamanijohn123@gmail.com>
2024-12-30 14:57:57 +0100

    is lower

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    print_alphabet_x10()
+    let mut r = is_lower(b'H');
+    print!("{r}");
+    r = is_lower(b'o');
+    print!("{r}");
+    r = is_lower(108);
+    print!("{r}");
+    print!("\n");
 }
 
 fn print_alphabet() {
@@ -13,4 +19,11 @@ fn print_alphabet_x10() {
     for _i in 0..10 {
         print_alphabet();
     }
+}
+
+fn is_lower(c: u8) -> u8 {
+    if c >= b'a' && c <= b'z' {
+        return 1;
+    }
+    0
 }
