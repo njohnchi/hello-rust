422985f32b179630365955885d17b2c8cb6b42a5
Nnamani John <nnamanijohn123@gmail.com>
2025-01-02 11:28:44 +0100

    print nine times table

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
 fn main() {
-    jack_bauer();
+    times_table();
 }
 
-fn jack_bauer() {
-    for i in 0..24 {
-        for j in 0..60 {
-            // println!("{i}:{j}");
-
-            // if i < 10 {
-            //     print!("0{i}:");
-            // } else {
-            //     print!("{i}:");
-            // }
-            // if j < 10 {
-            //     print!("0{j}\n");
-            // } else {
-            //     print!("{j}\n");
-            // }
-
-            println!("{i:02}:{j:02}");
+fn times_table() {
+    for i in 0..=9 {
+        for j in 0..=9 {
+            if j == 0 {
+                print!("{}", i * j);
+                continue;
+            }
+            print!(",{:3}", i * j);
         }
+        print!("\n");
     }
 }
