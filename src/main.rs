78f82f27904b369d2f07265a69db22a93a8de6b3
Nnamani John <nnamanijohn123@gmail.com>
2025-01-02 12:55:20 +0100

    print sum of multiples of 3 or 5

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
 fn main() {
-    print_times_table(3);
-    print!("\n");
-    print_times_table(5);
-    print!("\n");
-    print_times_table(98);
-    print!("\n");
-    print_times_table(12);
-}
-
-fn print_times_table(n: i32) {
-    if n < 0 || n > 15 {
-        return;
-    }
-
-    for i in 0..=n {
-        for j in 0..=n {
-            if j == 0 {
-                print!("{}", i * j);
-                continue;
-            }
-            print!(",{:3}", i * j);
+    let mut sum = 0;
+    for i in 0..1024 {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
         }
-        print!("\n");
     }
+    println!("{sum}");
 }
