c84a54dc3092ba50215e271788c05c4c0beb28c4
Nnamani John <nnamanijohn123@gmail.com>
2025-01-02 12:41:56 +0100

    print to 98

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 fn main() {
-    println!("{}", add(4, 7));
-    println!("{}", add(-4, 7));
-    println!("{}", add(0, 0));
-    println!("{}", add(4, -7));
-    println!("{}", add(-4, -7));
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
 }
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
+fn print_to_98(n: i32) {
+    if n > 98 {
+        for i in (98..=n).rev() {
+            if i == n {
+                print!("{i}");
+                continue;
+            }
+            print!(", {i}");
+        }
+        print!("\n");
+    } else {
+        for i in n..=98 {
+            if i == n {
+                print!("{i}");
+                continue;
+            }
+            print!(", {i}");
+        }
+        print!("\n");
+    }
 }
