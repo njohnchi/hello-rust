1536279f9dbf67d9bf8c7d29d58d68eb58b96ec6
Nnamani John <nnamanijohn123@gmail.com>
2025-01-02 11:07:33 +0100

    print 24 hours

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,24 @@
 fn main() {
-    print_last_digit(37);
-    print_last_digit(0);
-    let r = print_last_digit(-34564);
-    println!("{r}");
+    jack_bauer();
 }
 
-fn print_alphabet() {
-    for i in 0..26 {
-        print!("{}", (b'a' + i) as char);
-    }
-    print!("\n");
-}
+fn jack_bauer() {
+    for i in 0..24 {
+        for j in 0..60 {
+            // println!("{i}:{j}");
 
-fn print_alphabet_x10() {
-    for _i in 0..10 {
-        print_alphabet();
-    }
-}
+            // if i < 10 {
+            //     print!("0{i}:");
+            // } else {
+            //     print!("{i}:");
+            // }
+            // if j < 10 {
+            //     print!("0{j}\n");
+            // } else {
+            //     print!("{j}\n");
+            // }
 
-fn is_lower(c: u8) -> u8 {
-    if c >= b'a' && c <= b'z' {
-        return 1;
+            println!("{i:02}:{j:02}");
+        }
     }
-    0
-}
-
-fn is_alpha(c: u8) -> u8 {
-    if c >= b'a' && c <= b'z' {
-        return 1;
-    }
-    if c >= b'A' && c <= b'Z' {
-        return 1;
-    }
-    0
-}
-
-fn print_sign(num: i64) -> i8 {
-    if num > 0 {
-        println!("+");
-        return 1;
-    }
-    if num < 0 {
-        println!("-");
-        return -1;
-    }
-
-    println!("0");
-    0
-}
-
-fn abs(num: i64) -> u64 {
-    if num < 0 {
-        return -num as u64;
-    }
-    num as u64
-}
-
-fn print_last_digit(num: i64) -> u64 {
-    let mut last_digit = num % 10;
-    if last_digit < 0 {
-        last_digit *= -1;
-    }
-    println!("{last_digit}");
-    last_digit as u64
 }
