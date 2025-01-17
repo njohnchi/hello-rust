77f1045029f0188c0ebd6feb26415e131a4c28dc
Nnamani John <nnamanijohn123@gmail.com>
2025-01-17 15:05:06 +0100

    Rock Paper Scissors!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 fn main() {
-    println!("{}", basic_op('+', 4, 7));
-    println!("{}", basic_op('-', 11, 8));
-    println!("{}", basic_op('*', 2, 6));
-    println!("{}", basic_op('/', 45, 5));
+    println!("{}", rps("rock", "scissors"));
+    println!("{}", rps("scissors", "rock"));
+    println!("{}", rps("rock", "rock"));
 }
 
-fn basic_op(operator: char, value1: i32, value2: i32) -> i32 {
-    match operator {
-        '+' => value1 + value2,
-        '-' => value1 - value2,
-        '*' => value1 * value2,
-        '/' => value1 / value2,
-        _ => panic!("wrong operator")
+fn rps(p1: &str, p2: &str) -> &'static str  {
+    match p1 {
+        "rock" => {
+            match p2 {
+                "rock" => "Draw!",
+                "paper" => "Player 2 won!",
+                "scissors" => "Player 1 won!",
+                _ => panic!("Invalid option")
+            }
+        },
+        "paper" => {
+            match p2 {
+                "rock" => "Player 1 won!",
+                "paper" => "Draw!",
+                "scissors" => "Player 2 won!",
+                _ => panic!("Invalid option")
+            }
+        },
+        "scissors" => {
+            match p2 {
+                "rock" => "Player 2 won!",
+                "paper" => "Player 1 won!",
+                "scissors" => "Draw!",
+                _ => panic!("Invalid option")
+            }
+        },
+        _ => panic!("Invalid option")
     }
 }
