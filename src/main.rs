3bd72eb271300798dd38ca5312b3b8cafa520a08
Nnamani John <nnamanijohn123@gmail.com>
2025-01-17 15:08:09 +0100

    refactor Rock Paper Scissors!

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,9 @@ fn main() {
 }
 
 fn rps(p1: &str, p2: &str) -> &'static str  {
-    match p1 {
-        "rock" => {
-            match p2 {
-                "rock" => "Draw!",
-                "paper" => "Player 2 won!",
-                "scissors" => "Player 1 won!",
-                _ => panic!("Invalid option")
-            }
-        },
-        "paper" => {
-            match p2 {
-                "rock" => "Player 1 won!",
-                "paper" => "Draw!",
-                "scissors" => "Player 2 won!",
-                _ => panic!("Invalid option")
-            }
-        },
-        "scissors" => {
-            match p2 {
-                "rock" => "Player 2 won!",
-                "paper" => "Player 1 won!",
-                "scissors" => "Draw!",
-                _ => panic!("Invalid option")
-            }
-        },
-        _ => panic!("Invalid option")
+    match (p1, p2) {
+        ("rock", "scissors") | ("paper", "rock") | ("scissors", "paper") => "Player 1 won!",
+        ("rock", "paper") | ("paper", "scissors") | ("scissors", "rock") => "Player 2 won!",
+        _ => "Draw!"
     }
 }
