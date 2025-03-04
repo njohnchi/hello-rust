b8ba75d7ddf13847fcfca78aebedc07984e484d6
Nnamani John <nnamanijohn123@gmail.com>
2025-03-04 11:16:46 +0100

    dog struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
 fn main() {
-    let closure = return_closure();
-    call_twice(closure)
+    let my_dog = Dog::from(
+        String::from("Bingo"),
+        2.45,
+        String::from("Tommy")
+    );
+    my_dog.intro();
+
+    let mut your_dog = Dog::new();
+    your_dog.name = String::from("Pluto");
+    your_dog.age = 4.72;
+    your_dog.owner = String::from("Jane");
+    your_dog.intro();
+
+    let another_dog = Dog::new();
+    another_dog.intro();
 }
 
-fn return_closure() -> impl Fn() {
-    let s = String::from("a closure");
-    move || println!("{s}")
+struct Dog {
+    name: String,
+    age: f64,
+    owner: String,
 }
 
-fn call_twice(f: impl Fn()) {
-    f();
-    f();
+impl Dog {
+    fn new() -> Self {
+        Self {
+            name: String::new(),
+            age: 0.0,
+            owner: String::new(),
+        }
+    }
+
+    fn from(name: String, age: f64, owner: String) -> Self {
+        Self { name, age, owner }
+    }
+
+    fn intro(&self) {
+        println!("My name is {}, I am {}, and my owner is {}",
+                 self.name, self.age, self.owner);
+    }
 }
